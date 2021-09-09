f1551df98fea3a73bdfa7472360d9234fe5401ee
ana467 <anastassiajurina05@gmail.com>
2021-09-09 18:36:08 +0300

    end

--- Multiplication/js.js
+++ Multiplication/js.js
@@ -5,6 +5,8 @@ class Calc	{
 	#input
 	#buttons
 
+	allowedKeys = [8, 13, 111, 106, 107, 109, 46, 37, 38, 39, 40]
+
 	constructor(containerID) {
 		
 
@@ -14,6 +16,7 @@ class Calc	{
 
 		try{
 		this.#input = this.#container.querySelector('input[type="text"]')
+		this.attachEventToInput()
 
 		this.#buttons = this.#container.querySelectorAll('.btn')
 		this.attachEventToButtons()
@@ -55,12 +58,48 @@ class Calc	{
 			})
 		} 
 	}
+// key
+	attachEventToInput(){
+	this.#input.addEventListener('keydown', event => {
+		
+		if((event.keyCode >= 48 && event.keyCode <= 57 ) 
+		|| 
+			(event.keyCode >= 96 && event.keyCode <= 105))
+		{
+				console.log(event.keyCode)
+		} else if (this.allowedKeys.includes(event.keyCode))
+		{
+				console.log(event.keyCode)
+				if(event.keyCode ==  13) this.showresult()
+		}
+
+		else {
+			event.preventDefault() // 
+		}
+
+
+		
+	// 		if(event.ctrlKey == true && event.key == 'a'){
+	// 			console.log(event)
+	// 		}
+	// })
+
+	/*this.#input.addEventListener('keyup', event => {
+		console.log('keyup')
+	})
+
+	this.#input.addEventListener('keypress', event => {
+		console.log('keypress')
+	})*/
+})
+	}
+
 	showresult() {
-		this.#input.value = 0
+		this.#input.value = eval(this.#input.value)
 	}
 
 	zeroFormula(){
-		this.#input.value = eval(this.#input.value)
+		this.#input.value = 0
 	}
 
 

8bca2e2ef701733ba654e7cf7d9bb557f5608efa
Darina Kopenkova <darisha_92@mail.ru>
2021-09-10 14:57:14 +0300

    ple
    Signed-off-by: Darina Kopenkova <darisha_92@mail.ru>

--- Multiplication/js.js
+++ Multiplication/js.js
@@ -1,87 +1,67 @@
+class Calc {
+  #container;
+  #input;
+  #buttons;
 
-class Calc	{
+  constructor(containerID) {
+    this.#container = document.getElementById(containerID);
 
-	#container 
-	#input
-	#buttons
+    if (this.#container == null) console.log("Check name");
 
-	constructor(containerID) {
-		
+    this.#input = this.#container.querySelector('input[type="text"]');
 
-		this.#container = document.getElementById(containerID)
+    this.#buttons = this.#container.querySelectorAll(".btn");
+    this.attachEventToButtons();
+  }
 
-		if(this.#container == null) console.log('Check name')
+  attachEventToButtons() {
+    if (typeof this.#buttons == "object" && this.#buttons.length > 0) {
+      this.#buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+          //console.log(event.target.innerText)
 
-		try{
-		this.#input = this.#container.querySelector('input[type="text"]')
+          let btnName = event.target.innerText;
+          if (btnName == "=") {
+            this.showresult();
+          } else if (btnName == "CE") {
+            this.zeroFormula();
+          } else {
+            if (this.clearFormula(btnName) == true) {
+              this.#input.value += btnName;
+            }
+          }
+        });
+      });
+    }
+  }
+  showresult() {
+    this.#input.value = 0;
+  }
 
-		this.#buttons = this.#container.querySelectorAll('.btn')
-		this.attachEventToButtons()
-		} catch(info) 
-		{
-				console.log(info.message)
-		}
-		
-		
-	}
+  zeroFormula() {
+    this.#input.value = eval(this.#input.value);
+  }
 
+  clearFormula(currentSymbol) {
+    //doubles
+    let inputText = this.#input.value.toString().trim();
+    let lastSymbol = inputText.substr(inputText.length - 1, 1);
 
-	attachEventToButtons(){
+    if (
+      lastSymbol != currentSymbol &&
+      isNaN(currentSymbol) &&
+      isNaN(lastSymbol)
+    ) {
+      this.#input.value =
+        inputText.substr(0, inputText.length - 1) + currentSymbol;
+    } else if (
+      (lastSymbol != currentSymbol && isNaN(currentSymbol)) ||
+      !isNaN(currentSymbol)
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
 
-		if(typeof this.#buttons== 'object'&& this.#buttons.length > 0) {
-
-			this.#buttons.forEach(button => {
-
-				button.addEventListener('click', event => {
-					//console.log(event.target.innerText)
-					
-					let btnName = event.target.innerText
-					if (btnName == '=')
-					{
-						this.showresult()
-					} else if(btnName == 'CE') {
-						this.zeroFormula()
-					} else
-					{
-
-						if(this.clearFormula(btnName) == true) {
-
-							this.#input.value += btnName
-						}
-						
-					}
-					
-				})
-			})
-		} 
-	}
-	showresult() {
-		this.#input.value = 0
-	}
-
-	zeroFormula(){
-		this.#input.value = eval(this.#input.value)
-	}
-
-
-	clearFormula(currentSymbol){
-		//doubles
-		let inputText = this.#input.value.toString().trim()
-		let lastSymbol = inputText.substr(inputText.length - 1, 1)
-
-		if(lastSymbol != currentSymbol && isNaN(currentSymbol) && isNaN(lastSymbol)) {
-
-			this.#input.value = inputText.substr(0, inputText.length - 1) + currentSymbol
-
-		} 
-		else if((lastSymbol != currentSymbol && isNaN(currentSymbol)) || !isNaN(currentSymbol))
-		{
-			return true
-		} 
-			return false
-		}
-	}
-
-
-new Calc('calc')
-
+const calc = new Calc("calc");

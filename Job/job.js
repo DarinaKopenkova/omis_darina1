b118e1f0c6c3964268d509405006c32d41ce0d75
Darina Kopenkova <darisha_92@mail.ru>
2021-09-16 14:09:24 +0300

    add random colors

--- Job/job.js
+++ Job/job.js
@@ -1,37 +1,38 @@
-let loops = document.getElementById('loopsTotal')
-let btn = document.querySelector('button[type="button"]')
-let result = document.getElementById('result')
+let loops = document.getElementById("loopsTotal");
+let btn = document.querySelector('button[type="button"]');
+let result = document.getElementById("result");
 
+let colors = ["red", "blue", "pink", "yellow", "green", "black"];
 
-btn = addEventListener('click', event=> {
-	result.innerHTML = ''
-	if(loops.value>0){
-		loop(0, loops.value)
-	}
-	
-})
+btn = addEventListener("click", (event) => {
+  result.innerHTML = "";
+  if (loops.value > 0) {
+    loop(0, loops.value);
+  }
+});
 
-function	loop(start, end) {
+function loop(start, end) {
+  if (start >= end) {
+    console.log("loop end");
+    return;
+  } else {
+    console.log("index ", start);
 
-	if(start >= end)
-	{
-		console.log('loop end')
-		return;
-	} else {
-		console.log('index ', start)
+    let div = createDiv();
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    // div.style.backgroundColor =
+    //   colors[Math.floor(Math.random() * colors.length)];
+    div.style.margin = "5px";
+    div.classList.add("box");
+    result.innerHTML += div.outerHTML;
 
-			let div = createDiv()
-			div.style.backgroundColor = 'red'
-			div.style.margin = '5px'
-			div.classList.add('box')
-			result.innerHTML += div.outerHTML
-
-		loop(++start, end)
-	}
+    loop(++start, end);
+  }
 }
 
 function createDiv() {
-	return document.createElement('div')
-
+  return document.createElement("div");
 }
-

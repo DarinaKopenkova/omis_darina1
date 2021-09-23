bf1c24ee4ae6b09d22023b48484afd4e80cc5a38
Darina Kopenkova <darisha_92@mail.ru>
2021-09-23 13:07:04 +0300

    fixed a mistake

--- form with validation /js.js
+++ form with validation /js.js
@@ -1,7 +1,7 @@
 const name = document.getElementById("name");
 const password = document.getElementById("password");
 const form = document.getElementById("form");
-const error.Element = document.getElementById("error")
+const errorElement = document.getElementById("error");
 
 form.addEventListener("submit", (e) => {
   let messages = [];
@@ -15,8 +15,8 @@ form.addEventListener("submit", (e) => {
   if (password.value.length >= 20) {
     messages.push("Password must be less than 20 characters");
   }
-  if(password.value === 'password') {
-      messages.push('Password cannot be password')
+  if (password.value === "password") {
+    messages.push("Password cannot be password");
   }
 
   if (messages.length > 0) {
